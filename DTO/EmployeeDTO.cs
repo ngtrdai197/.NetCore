787a2800470e191cddce41e4cfc89a911e92c385
Nguyen Trong Dai <ngtrdai290197@gmail.com>
2019-01-08 02:20:42 +0700

    :construction: add EmployeeDTO

---
+++ DTO/EmployeeDTO.cs
@@ -0,0 +1,9 @@
+namespace NetCoreUsingVsCode.DTO
+{
+    public class EmployeeDTO
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
